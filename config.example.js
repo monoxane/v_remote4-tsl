0a1e65648d94ead90520b6922f96323884db68fb
Oliver Herrmann <oliver@monoxane.io>
2021-01-13 20:35:27 +1100

    feat: TSL and Rosstalk label updates

--- config.example.js
+++ config.example.js
@@ -1,8 +1,22 @@
 module.exports = {
     v_remote: {
-        ip: '10.10.10.10', // IP of a V_Remote Interface, only tested with 1G/1 MGMT interface but should work on the others.
-        port: 9000 // Ember+ Port, usually 9000 unless you've got some interesting stuff going on.
+        host: '10.10.10.10', // IP of a V_Remote Interface, only tested with 1G/1 MGMT interface but should work on the others.
+        port: 9000, // Ember+ Port, usually 9000 unless you've got some interesting stuff going on.
+        pipmap: [1, 2, 3, 4] // TSL Address to follow for tally updates, in order of which PIP in the MV the border will be displayed on.
     },
-    pipmap: [1, 2, 3, 4], // TSL Address to follow for tally updates, in order of which PIP in the MV the border will be displayed on.
+    tsl: {
+        enabled: false, // Set to true to send TSL label updates for the inputs
+        host: '10.10.10.11', // IP of the device to send label updates to
+        port: 5727, // Port to send TCP TSL v3.1 data to 
+        sourcemap: [5, 6, 7, 8], // TSL address to update, in order of the V_Remote4 Inputs (It's not possible to do this via outputs unfortunately)
+        defaults: ['VREM1', 'VREM2', 'VREM3', 'VREM4'] // Default labels to reset on exit
+    },
+    rosstalk: {
+        enabled: false, // Set to true to use the RossTalk MNEM command to update Carbonite Mnemonics
+        host: '10.10.10.11', // IP of the device to send mnemonic updates to
+        port: 7788, // Rosstalk Port (usually 7788)
+        sourcemap: [5, 6, 7, 8], // Input Number to update, in order of the V_Remote4 Inputs (It's not possible to do this via outputs unfortunately)
+        defaults: ['VREM1', 'VREM2', 'VREM3', 'VREM4'] // Default labels to reset on exit
+    },
     processingInterval: 20 // Delay on tally update interval, if set too low the vremote will fail to update, tested with 20ms.
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,142 @@
 var _ = require('lodash') // I really don't like using lodash for silly things but I need uniqBy()
 const UMD = require('tsl-umd')
 const EmberClient = require('node-emberplus').EmberClient
+const Net = require('net');
 
 // Get user configuration data
 const config = require('../config')
 
 let state = []
 let isConnectedToVRemote = false
-let queue = []
-let isProcessing = false 
+let isConnectedToTSL = false
+let isConnectedToRossTalk = false
+let MVqueue = []
+let isProcessingMV = false 
 let processingInterval
+let sourceID = []
 
 // The Ember+ Client for the V_Remote4 connection
-const vremote = new EmberClient(config.v_remote.ip, config.v_remote.port)
+const vremote = new EmberClient(config.v_remote.host, config.v_remote.port)
 
 // Initialise the TSL server
 const umd = new UMD(5001)
+
+// The TCP Clients
+const TSLClient = new Net.Socket()
+const RossTalkClient = new Net.Socket()
+
+/*
+  ROSSTALK LOGIC
+*/
+
+const connectRossTalk = () => new Promise(resolve => {
+if (config.rosstalk.enabled) {
+    RossTalkClient.connect({ port: config.rosstalk.port, host: config.rosstalk.host }, () => {
+      console.log(`RossTalk Connected to ${config.tsl.host}:${config.rosstalk.port}`)
+      isConnectedToRossTalk = true
+      resolve()
+    })
+
+    RossTalkClient.on('data', chunk => {
+      console.log(`Data received from the server: ${chunk.toString()}.`)
+    })
+
+    RossTalkClient.on('end', () => {
+      console.log('Requested an end to the TCP connection')
+    })
+
+    RossTalkClient.on('close', () => {
+      console.log('Requested an end to the TCP connection')
+    })
+
+    RossTalkClient.on('error', e => {
+      console.log(e)
+    })
+  } else resolve()
+})
+
+const processRossTalk = () => {
+  if (config.rosstalk.enabled && isConnectedToRossTalk) {
+    sourceID.map((id, index) => {
+      if (id) {
+        console.log(`Updating RossTalk mnemonic for address ${config.rosstalk.sourcemap[index]} to ${id.replace(/^\s+|\s+$/g,'').padEnd(16).slice(0, 16)}`)
+        console.log(`MNEM IN:${config.rosstalk.sourcemap[index]}:${id.replace(/^\s+|\s+$/g,'').slice(0, 8)}`)
+        RossTalkClient.write(`MNEM IN:${config.rosstalk.sourcemap[index]}:${id.replace(/^\s+|\s+$/g,'').slice(0, 8)}\n`)
+      }
+    })
+  }
+}
+
+const handleRossTalkExit = () => new Promise(resolve => {
+  if (config.rosstalk.enabled && isConnectedToRossTalk) {
+    sourceID.map((id, index) => {
+      if (id) {
+        console.log(`Updating RossTalk mnemonic for address ${config.rosstalk.sourcemap[index]} to ${config.rosstalk.defaults[index]}`)
+        RossTalkClient.write(`MNEM IN:${config.rosstalk.sourcemap[index]}:${config.rosstalk.defaults[index]}\n`)
+      }
+    })
+    RossTalkClient.end()
+  }
+  resolve()
+})
+
+/*
+  TSL LOGIC
+*/
+
+const connectTSL = () => new Promise((resolve, reject) => {
+  if (config.tsl.enabled) {
+    TSLClient.connect({ port: config.tsl.port, host: config.tsl.host }, () => {
+      console.log(`TSL Connected to ${config.tsl.host}:${config.tsl.port}`)
+      isConnectedToTSL = true
+      resolve()
+    })
+
+    TSLClient.on('data', chunk => {
+      console.log(`Data received from the server: ${chunk.toString()}.`)
+    })
+
+    TSLClient.on('end', () => {
+      console.log('Requested an end to the TCP connection')
+    })
+
+    TSLClient.on('error', e => {
+      console.log(e)
+    })
+  } else resolve()
+})
+
+const processTSL = () => {
+  if (config.tsl.enabled && isConnectedToTSL) {
+    sourceID.map((id, index) => {
+      if (id) {
+        console.log(`Updating TSL for address ${config.tsl.sourcemap[index]} to ${id.replace(/^\s+|\s+$/g,'').padEnd(16).slice(0, 16)}`)
+        const packet = Buffer.from([ ...Buffer.from((0x80 + config.tsl.sourcemap[index].toString(16)), 'hex'), 00, ...Buffer.from(id.replace(/^\s+|\s+$/g,'').padEnd(16).slice(0, 16)) ])
+        TSLClient.write(packet)
+      }
+    })
+  }
+}
+
+const handleTSLExit = () => new Promise(resolve => {
+  if (config.tsl.enabled && isConnectedToTSL) {
+    if (config.rosstalk.enabled && isConnectedToRossTalk) {
+      sourceID.map((id, index) => {
+        if (id) {
+          console.log(`Updating TSL for address ${config.tsl.sourcemap[index]} to ${config.tsl.defaults[index].replace(/^\s+|\s+$/g,'').padEnd(16).slice(0, 16)}`)
+          const packet = Buffer.from([ ...Buffer.from((0x80 + config.tsl.sourcemap[index].toString(16)), 'hex'), 00, ...Buffer.from(config.tsl.defaults[index].replace(/^\s+|\s+$/g,'').padEnd(16).slice(0, 16)) ])
+          TSLClient.write(packet)
+        }
+      })
+    }
+    TSLClient.end()
+    resolve()
+  } else resolve()
+})
+
+/*
+  VREMOTE LOGIC
+*/
 
 /**
  * Returns border colour
@@ -31,28 +152,28 @@ const getBorderColour = status => {
   } else return 0
 }
 
-// Process the current queue data in order
-const processQueue = () => {
-  if (queue.length !=0) {
-    const currentData = queue.pop() // Update from the end of the queue (FIFO)
-    const currentState = state[config.pipmap.indexOf(currentData.address)]
-    console.log(`Update PIP ${config.pipmap.indexOf(currentData.address) + 1} (${currentData.label}) to ${currentState.red ? 'Red' : currentState.green ? 'Green' : 'Clear'}`)
-    vremote.setValue(vremote.root.getElementByPath(`1.7.${config.pipmap.indexOf(currentData.address) + 1}.13`), getBorderColour(currentState)) // Set border colour to tally data
+// Process the current MVqueue data in order
+const processMVqueue = () => {
+  if (MVqueue.length !=0) {
+    const currentData = MVqueue.pop() // Update from the end of the MVqueue (FIFO)
+    const currentState = state[config.v_remote.pipmap.indexOf(currentData.address)]
+    console.log(`Update PIP ${config.v_remote.pipmap.indexOf(currentData.address) + 1} (${currentData.label.replace(/^\s+|\s+$/g,'')}) to ${currentState.red ? 'Red' : currentState.green ? 'Green' : 'Clear'}`)
+    vremote.setValue(vremote.root.getElementByPath(`1.7.${config.v_remote.pipmap.indexOf(currentData.address) + 1}.13`), getBorderColour(currentState)) // Set border colour to tally data
       .catch(console.log)
   } else {
-    // Clear the interval if the queue is empty to stop wasting CPU
+    // Clear the interval if the MVqueue is empty to stop wasting CPU
     clearInterval(processingInterval)
-    isProcessing = false
+    isProcessingMV = false
   }
 }
 
 /**
- * Queues an update
+ * MVqueues an update
  * @param {object} tally - The tally update object
  */
-const queueTally = tally => {
-  queue.push(tally)
-  _.uniqBy(queue, tally => tally.address)
+const MVqueueTally = tally => {
+  MVqueue.push(tally)
+  _.uniqBy(MVqueue, tally => tally.address)
 }
 
 // Handle Errors
@@ -61,9 +182,9 @@ umd.server.on('error', console.log)
 // Recieve TSL data and process it
 umd.on('message', tally => {
   // Need to process on a loop to ensure V_Remote4 actually takes the updates
-  if (!isProcessing) {
-    processingInterval = setInterval(processQueue, config.processingInterval)
-    isProcessing = true
+  if (!isProcessingMV) {
+    processingInterval = setInterval(processMVqueue, config.processingInterval)
+    isProcessingMV = true
   }
 
   // Create an update object with the data we want in a readable format
@@ -77,11 +198,11 @@ umd.on('message', tally => {
   }
 
   // Ensure we are only updating borders for things we actually care about
-  if (config.pipmap.includes(tally.address)) {
-    state[config.pipmap.indexOf(tally.address)] = { ...updateObject } // Keep state
+  if (config.v_remote.pipmap.includes(tally.address)) {
+    state[config.v_remote.pipmap.indexOf(tally.address)] = { ...updateObject } // Keep state
     if (isConnectedToVRemote) {
-      // Queue the change if currently connected
-      queueTally(tally)
+      // MVqueue the change if currently connected
+      MVqueueTally(tally)
     } else {
       // Log if not connected
       console.log('NOT CONNECTED TO VREMOTE')
@@ -89,9 +210,10 @@ umd.on('message', tally => {
   }
 })
 
-// The Ember+ Tree must be updated to correctly enable borders on the MV head
-vremote.connect()
+const connectVremote = () => new Promise((resolve, reject) => {
+  vremote.connect()
   .then(() => vremote.getDirectory())
+  // The Ember+ Tree must be updated to correctly enable borders on the MV head
   .then(() => vremote.getElementByPath('1.7.1.12'))
   .then(() => vremote.setValue(vremote.root.getElementByPath('1.7.1.12'), true))
   .then(() => vremote.getElementByPath('1.7.2.12'))
@@ -100,21 +222,53 @@ vremote.connect()
   .then(() => vremote.setValue(vremote.root.getElementByPath('1.7.3.12'), true))
   .then(() => vremote.getElementByPath('1.7.4.12'))
   .then(() => vremote.setValue(vremote.root.getElementByPath('1.7.4.12'), true))
+  // Getting current source IDs
+  .then(() => vremote.getElementByPath('1.15.1.1.1.4'))
+  .then(node => sourceID[0] = node.contents.value.split('sourceId=')[1]?.replace(/(\r\n|\n|\r)/gm, ""))
+  .then(() => vremote.getElementByPath('1.15.1.1.2.4'))
+  .then(node => sourceID[1] = node.contents.value.split('sourceId=')[1]?.replace(/(\r\n|\n|\r)/gm, ""))
+  .then(() => vremote.getElementByPath('1.15.1.1.3.4'))
+  .then(node => sourceID[2] = node.contents.value.split('sourceId=')[1]?.replace(/(\r\n|\n|\r)/gm, ""))
+  .then(() => vremote.getElementByPath('1.15.1.1.4.4'))
+  .then(node => sourceID[3] = node.contents.value.split('sourceId=')[1]?.replace(/(\r\n|\n|\r)/gm, ""))
+  .then(() => console.log(sourceID))
   .then(() => isConnectedToVRemote = true)
+  .catch(reject)
+  .finally(resolve)
+})
 
 // Set all borders to black on exit
-const handleExit = () => {
-  console.log('\r')
-  console.log('EXITING')
+const handleVRemoteExit = () => new Promise(resolve => {
   vremote.setValue(vremote.root.getElementByPath('1.7.1.13'), 0)
   .then(() => vremote.setValue(vremote.root.getElementByPath('1.7.2.13'), 0))
   .then(() => vremote.setValue(vremote.root.getElementByPath('1.7.3.13'), 0))
   .then(() => vremote.setValue(vremote.root.getElementByPath('1.7.4.13'), 0))
   .then(() => vremote.disconnect())
   .catch(e => console.log(e))
-  .finally(() => process.exit(0))
+  .finally(resolve)
+})
+
+/*
+  MAIN
+*/
+
+// Initalise Things
+connectVremote()
+  .then(() => connectTSL())
+  .then(() => connectRossTalk())
+  .then(() => processTSL())
+  .then(() => processRossTalk())
+
+// Handle clean exit
+const handleExit = () => {
+  console.log('\r')
+  console.log('EXITING')
+  handleVRemoteExit()
+    .then(handleTSLExit())
+    .then(handleRossTalkExit())
+    .finally(() => process.exit(0))
 }
 
-// Handle SIGTERM and SIGINT to clear borders
+// Catch SIGTERM and SIGINT
 process.on('SIGTERM', () => handleExit())
 process.on('SIGINT', () => handleExit())
